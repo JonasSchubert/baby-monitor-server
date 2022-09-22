541b63d8202195841b0c98651cfa158ef43c28c3
Jonas  Schubert <jonas.schubert.projects@web.de>
2022-09-22 21:22:31 +0200

    feat(server): implement sensor climate

--- server.js
+++ server.js
@@ -35,14 +35,20 @@ app.get('/api/v1/climate', (_, response) => {
   const cpuTemperature = +require('child_process').execSync('cat /sys/class/thermal/thermal_zone0/temp') / 1000;
   const gpuTemperature = +(/[0-9]{1,4}\.[0-9]{1,4}/.exec(require('child_process').execSync('vcgencmd measure_temp').toString())[0]);
 
+  const sensorClimate = require('child_process').execSync('python climate.py').toString();
+  const sensorTemperature = +(/Temperature\:\s[0-9]{1,4}\.[0-9]{1,2}/.exec(sensorClimate)[0].replace('Temperature: ', ''));
+  const sensorHumidity = +(/Humidity\:\s[0-9]{1,4}\.[0-9]{1,2}/.exec(sensorClimate)[0].replace('Humidity: ', ''));
+
   response.status(200).send({
     humidity: {
-      sensor: null
+      sensor: sensorHumidity,
+      unit: '%'
     },
     temperature: {
       cpu: cpuTemperature,
       gpu: gpuTemperature,
-      sensor: null
+      sensor: sensorTemperature,
+      unit: '°C'
     }
   });
 });
