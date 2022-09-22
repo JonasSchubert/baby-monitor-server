2ab6580060bcee61e41a894469ade5e80aa19297
Jonas  Schubert <jonas.schubert.projects@web.de>
2022-09-22 21:07:44 +0200

    feat(server): add endpoint for climate

--- server.js
+++ server.js
@@ -31,6 +31,22 @@ app.get('/video', function (req, res) {
   videoStream.pipe(res);
 });
 
+app.get('/api/v1/climate', (_, response) => {
+  const cpuTemperature = +require('child_process').execSync('cat /sys/class/thermal/thermal_zone0/temp') / 1000;
+  const gpuTemperature = +(/[0-9]{1,4}\.[0-9]{1,4}/.exec(require('child_process').execSync('vcgencmd measure_temp').toString())[0]);
+
+  response.status(200).send({
+    humidity: {
+      sensor: null
+    },
+    temperature: {
+      cpu: cpuTemperature,
+      gpu: gpuTemperature,
+      sensor: null
+    }
+  });
+});
+
 app.listen(8000, function () {
   console.log('Listening on port 8000!');
 });
