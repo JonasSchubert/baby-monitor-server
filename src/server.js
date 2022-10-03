9ffc90bacc44ebcaeb3993fa0ebcf1f9f78aa27c
Jonas Schubert <jonas.schubert.projects@web.de>
2022-10-03 18:23:15 +0200

    refactor(project): remove old source files to move to python

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { useClimate } = require('./climate/route');
-const { useStream } = require('./stream/route');
-
-const app = express();
-app.get('/', (_, response) => response.sendFile(__dirname + '/index.html'));
-useClimate(app);
-useStream(app);
-app.listen(8000, ()=> console.log('Listening on port 8000!'));
