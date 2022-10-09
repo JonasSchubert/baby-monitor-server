bab62bbf29c12b75d9755035288af939201c78ea
Jonas  Schubert <jonas.schubert.projects@web.de>
2022-10-09 10:24:57 +0200

    refactor(raspberry-pi-setup): remove obsolete commands

--- raspberry-pi-setup.sh
+++ raspberry-pi-setup.sh
@@ -6,27 +6,8 @@ sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
 
-# Install node v16
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt install -y nodejs
-sudo npm install -g npm
-
 # Install additional essential packages
 sudo apt install -y build-essential git
-
-# Install docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-# sudo usermod -aG docker ${USER} # Not working in script
-
-# Install docker-compose
-sudo apt-get install docker-compose-plugin
-
-# Install Respeaker
-cd ~
-git clone https://github.com/HinTak/seeed-voicecard.git
-cd seeed-voicecard
-sudo ./install.sh
 
 # Install Grove
 cd ~
